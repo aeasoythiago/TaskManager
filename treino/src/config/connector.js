c194542a80a463a0171321072599b59af182a6ad
Thiago Mecenas <thiagomecena@hotmail.com>
2025-12-11 13:20:17 -0300

    Adding docker and docker-compose

--- treino/src/config/connector.js
+++ treino/src/config/connector.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-    
-const ligar = async () =>{
-    try {
-        await mongoose.connect(`mongodb+srv://${process.env.MONGODB_LOGIN}:${process.env.MONGODB_PASSWORD}@ttserver.gkubuda.mongodb.net/?retryWrites=true&w=majority&appName=TTSERVER`);
-        console.log("Servidor conectado com sucesso.\n");
-    } catch (error) {
-            console.log("Erro ao conectar com o banco de dados.\n");
-        }
-    }
 
-    module.exports = {ligar};
+const ligar = async () => {
+    try {
+        let dbURI = process.env.MONGO_URI;
+        if (!dbURI) {
+            dbURI = `mongodb+srv://${process.env.MONGODB_LOGIN}:${process.env.MONGODB_PASSWORD}@ttserver.gkubuda.mongodb.net/?retryWrites=true&w=majority&appName=TTSERVER`;
+        }
+        await mongoose.connect(dbURI);
+        console.log("Servidor conectado com sucesso (URI: " + (process.env.MONGO_URI ? "Docker/Local" : "Atlas/Nuvem") + ").\n");
+    } catch (error) {
+        console.log("Erro ao conectar com o banco de dados:", error.message);
+    }
+}
+
+module.exports = { ligar };
